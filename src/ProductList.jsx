4c652d5200f522f643d282a746dbe637bf66c2af
Ngane Emmanuel <emmanuelngane06@gmail.com>
2025-05-25 09:00:20 -0400

    Task 4: Integrate Redux functionality in your components

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,13 +2,20 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import './ProductList.css';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const dispatch = useDispatch();
     const [addedToCart, setAddedToCart] = useState({});
+    const CartItems = useSelector((state) => state.cart.cart);
+
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
+
+    // dispatch(addItem(product));
 
     const plantsArray = [
         {
